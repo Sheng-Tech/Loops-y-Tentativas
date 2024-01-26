7773616aca922fde247935d455930e304f446ed6
Jesus Gomez <gomjesal@gmail.com>
2024-01-26 20:45:41 -0300

    Crea el desafio4.js

---
+++ desafio4.js
@@ -0,0 +1,8 @@
+let numero = prompt ("¡Por favor ingresa un número!"); 
+let contador = 0;
+
+while (numero >= contador) { 
+    alert (`Cuenta progresiva en: ${contador}`);
+    console.log (contador);
+    contador++; 
+}
